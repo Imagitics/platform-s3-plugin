438743985b3f583e5ce407e5bec7d0b439e52481
nikhilbhide <nikhilbhide15@gmail.com>
2020-03-12 20:15:11 +0530

    added example to upload entity to s3 bucket using configuration

--- app/s3-plugin/main.go
+++ app/s3-plugin/main.go
@@ -1,28 +1,6 @@
 package main
 
-import (
-	"fmt"
-	"github.com/nik/Imagitics/platform-s3-plugin/client"
-	"github.com/nik/Imagitics/platform-s3-plugin/utility"
-	"path/filepath"
-)
+import ()
 
 func main() {
-	bigBuff := make([]byte, 750)
-	configPath := filepath.FromSlash("/app/platform-s3-plugin/config/config.json")
-
-	config, error := utility.LoadConfiguration(configPath)
-	if error != nil {
-		panic(error)
-	}
-
-	s3Service, err := client.NewS3Service(config.S3.AwsAccessKey, config.S3.AwsSecretKey, config.S3.Region, "")
-	if err == nil {
-		s3Location, err := s3Service.Upload("tenant-id-pkg1234", "path", bigBuff)
-		if err == nil {
-			fmt.Println(s3Location)
-		} else {
-			fmt.Println(err)
-		}
-	}
 }
