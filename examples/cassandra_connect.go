d23a94d23c0055958bb68c89324667cbce6a87ef
nikhilbhide <nikhilbhide15@gmail.com>
2020-03-15 17:04:06 +0530

    added example for connecting to cassandra

--- examples/cassandra_connect.go
+++ examples/cassandra_connect.go
@@ -1,1 +1,18 @@
 package main
+
+import "github.com/nik/Imagitics/platform-s3-plugin/infra/cassandra"
+
+func main() {
+	conn:= cassandra.CassandraConn{
+		Host:"cassandra.us-east-1.amazonaws.com",
+		Port: "9142",
+		User:"CassandraMS-at-948987282987",
+		Password:"HNA4B1211FjKoC7Xb2eetbfC8gyFvEw0WXEM180QT9c=",
+		Consistency: "Quorum",
+		Keyspace:"platform_s3_db",
+	}
+
+
+	cassandra:= conn.InitSession()
+	cassandra.Close()
+}
