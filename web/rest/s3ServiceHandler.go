7fc2cbf60f4f258f34875ec6644c18bd9c3e8c59
nikhilbhide <nikhilbhide15@gmail.com>
2020-03-17 15:50:59 +0530

    refactored code

--- web/rest/s3ServiceHandler.go
+++ web/rest/s3ServiceHandler.go
@@ -9,6 +9,7 @@ import (
 	"github.com/nik/Imagitics/platform-s3-plugin/metadata/repository"
 	"github.com/nik/Imagitics/platform-s3-plugin/web/rest/model"
 	"io/ioutil"
+	"mime/multipart"
 	"net/http"
 	"os"
 )
@@ -39,65 +40,34 @@ func (handler *S3FileHandler) S3UploadHandler(w http.ResponseWriter, r *http.Req
 	vars := mux.Vars(r)
 	tenantId := vars["tenant_id"]
 
-	//validateTenantId - to be added
-
 	// Retrieve aws credentials for this tenant
-	handler.getAWSCredentialsByTenantId(tenantId)
+	s3Credentials, err := handler.getAWSCredentialsByTenantId(tenantId)
+	if err != nil {
+		http.Error(w, "Bad request", http.StatusBadRequest)
+	}
 
 	// Set file limit to configurable size
 	r.Body = http.MaxBytesReader(w, r.Body, 2*1024*1024) // 2 Mb
+
 	// Unmarshal the request body into struct and then perform the option of upload
 	// Retrieve request and file from the form
-
-	request := r.FormValue("request")
-	file, header, err := r.FormFile("entity")
+	s3UploadRequest, err := validateAndRetriveUploadRequest(r.FormValue("request"))
 	if err != nil {
-		switch err.Error() {
-		case FileSizeLimitError:
-			http.Error(w, "Exceeded allowed file size limit of 2 Mb", http.StatusBadRequest)
-			return
-		case NoSuchFileError:
-			http.Error(w, "Bad file", http.StatusBadRequest)
-			return
-		}
-
-		defer file.Close()
-		// read all of the contents of our uploaded file into a byte array
-		fileBytes, err := ioutil.ReadAll(file)
-		if err != nil {
-			fmt.Println(err)
-		}
-
-		if file == nil {
-			fmt.Println("File to be uploaded to s3 can not be empty")
-		}
-
-		// Create a temporary file within our temp-images directory that follows
-		// a particular naming pattern
-		tempFile, err := ioutil.TempFile("", header.Filename)
-		tempFile.Write(fileBytes)
-
-		if err != nil {
-			fmt.Println(err)
-		}
-
-		// This will remove the file once its uploaded to s3 bucket
-		defer os.Remove(tempFile.Name())
-
-		s3UploadRequest := model.S3UploadRequest{}
-		err = json.Unmarshal([]byte(request), &s3UploadRequest)
-		if err == nil {
-			w.WriteHeader(402)
-		}
-		//s3UploadRequest.File = file
-
-		if err != nil {
-			fmt.Println(err)
-			return
-		}
-
-		fmt.Println(s3UploadRequest.Bucket)
+		http.Error(w, err.Error(), http.StatusBadRequest)
 	}
+
+	// Validate the uploaded file and retrieve the file handler for uploading the physical file to s3
+	tempFile, err := validateAndGetFileHandler(r.FormFile("entity"))
+	if err != nil {
+		// File validation failed
+		http.Error(w, err.Error(), http.StatusBadRequest)
+	}
+
+	// All validations are passed. Now a file can be uploaded to s3.
+	// This will remove the file once its uploaded to s3 bucket
+	defer os.Remove(tempFile.Name())
+	//s3UploadRequest.File = tempFile
+
 }
 
 // getAWSCredentialsByTenantId retrieves aws credentials for the provided tenant identifier.
@@ -119,4 +89,62 @@ func (handler *S3FileHandler) getAWSCredentialsByTenantId(tenantId string) (*cli
 	}
 
 	return s3Credentials, nil
+}
+
+// validateAndGetFileHandler performs validation on uploaded file.
+// File can not be nil and there is a limit on the file size.
+// If all file validations are passed, it creates temporary file at /tmp location
+// This physical file (stored on disk) is to be used later to upload
+func validateAndGetFileHandler(file multipart.File, header *multipart.FileHeader, err error) (*os.File, error) {
+	if file == nil {
+		// file object must not be nil
+		return nil, errors.New("File to be uploaded to s3 can not be empty")
+	}
+
+	if err != nil {
+		// check for error and corresponding cause
+		switch err.Error() {
+		case FileSizeLimitError:
+			return nil, errors.New("Exceeded allowed file size limit of 2 Mb")
+		case NoSuchFileError:
+			return nil, errors.New("Bad file")
+		}
+	}
+
+	// Read all of the contents of our uploaded file into a byte array
+	fileBytes, err := ioutil.ReadAll(file)
+	if err != nil {
+		return nil, err
+	}
+	// create a temporary file within our temp-images directory that follows
+	// a particular naming pattern
+	tempFile, err := ioutil.TempFile("", header.Filename)
+	if err != nil {
+		return nil, err
+	}
+	tempFile.Write(fileBytes)
+
+	return tempFile, nil
+}
+
+// validateAndRetriveUploadRequest validates the uploaded file upload request
+// It retrieves tenant identifier and bucket attributes from the request
+func validateAndRetriveUploadRequest(body string) (*model.S3UploadRequest, error) {
+	// initialize struct instance and try to retrieve request object from the input form paramter
+	s3UploadRequest := &model.S3UploadRequest{}
+	err := json.Unmarshal([]byte(body), &s3UploadRequest)
+	if err != nil {
+		//error in unmarshaling because of format mismatch
+		return nil, err
+	}
+
+	if s3UploadRequest.Bucket == "" {
+		//validate empty bucket case
+		return nil, errors.New("Bucket can not be empty")
+	} else if s3UploadRequest.TenantId == "" {
+		//validate tenant identifier case
+		return nil, errors.New("Invalid request")
+	}
+
+	return s3UploadRequest, nil
 }
