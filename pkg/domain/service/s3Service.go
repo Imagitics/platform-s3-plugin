ed9ffc077e6ecd55f947e1b017071d60e268e3c7
nikhilbhide <nikhilbhide15@gmail.com>
2020-05-01 17:24:04 +0530

    added domain service object

--- pkg/domain/service/s3Service.go
+++ pkg/domain/service/s3Service.go
@@ -1,31 +1,46 @@
-package domain
+package service
 
 import (
 	"bytes"
+	"errors"
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/awserr"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/s3"
 	"github.com/aws/aws-sdk-go/service/s3/s3manager"
+	"github.com/nik/Imagitics/platform-s3-plugin/metadata/repository"
 	"github.com/nik/Imagitics/platform-s3-plugin/pkg/model"
 )
 
 type S3Service struct {
 	s3Client *s3.S3
 	session  *session.Session
+	repo repository.S3Metadata
 }
 
 // Initializes s3Client with required parameters such as accesskey, secretkey and region
 // Token is used for aws session token
 // It returns pointer to the object that wraps s3Client
-func NewS3Service(awsAccessKey string, awsSecretKey string, region string, token string) (*S3Service, error) {
-	s3Cred, err := model.NewS3Credential(awsAccessKey, awsSecretKey, token)
+func NewS3Service(tenantID string, region string, repo repository.S3Metadata, token string) (*S3Service, error) {
+
+	//retrieve aws metadata stored agains tenant-id
+	s3Credentials, s3Metadata, err := getAWSCredentialsAndMetadataByTenantId(tenantID, repo)
 	if err != nil {
 		return nil, err
 	}
 
+	//instantiate aws credentials model
+	s3Cred, err := model.NewS3Credential(s3Credentials.AWSAccessKey, s3Credentials.AWSSecretKey, token)
+	if err != nil {
+		return nil, err
+	}
+
+	if(region=="") {
+		region = s3Metadata.Region
+	}
+
 	// Provide region to initialize s3Client based on the supplied s3Credentials
-	s3Client, session, err := s3Cred.NewS3Client(region)
+	s3Client, session, err := s3Cred.NewS3Client(s3Metadata.Region)
 	if err != nil {
 		return nil, err
 	}
@@ -77,3 +92,25 @@ next:
 
 	return fileUploadReuslt.Location, nil
 }
+
+// getAWSCredentialsByTenantId retrieves aws credentials for the provided tenant identifier.
+func getAWSCredentialsAndMetadataByTenantId(tenantId string, repo repository.S3Metadata) (*model.S3Credentials, *model.S3Metadata, error) {
+	if tenantId == "" {
+		// tenantId can not be empty.Its better that the validation is done at a higher level
+		return nil, nil, errors.New("Tenant-ID can not be empty")
+	}
+
+	// Retrieve aws metadata
+	s3Metadata, err := repo.Get(tenantId)
+	if err != nil {
+		return nil, nil, err
+	}
+	// Populate data structure as per the attributes
+	s3Credentials := &model.S3Credentials{
+		AWSSecretKey: s3Metadata.SecretKey,
+		AWSAccessKey: s3Metadata.AccessKey,
+	}
+
+	return s3Credentials, s3Metadata, nil
+}
+
