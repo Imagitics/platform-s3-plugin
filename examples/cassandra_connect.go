9ef2f3359bc8cc8b3799a387d2df049412fe64c8
nikhilbhide <nikhilbhide15@gmail.com>
2020-05-10 11:17:55 +0530

    addressed code smell

--- examples/cassandra_connect.go
+++ examples/cassandra_connect.go
@@ -4,7 +4,7 @@ import "github.com/nik/Imagitics/platform-s3-plugin/infra/cassandra"
 
 func main() {
 	conn := cassandra.CassandraConn{
-		Hosts:        [] string {"172.18.0.2"},
+		Hosts:       []string{"172.18.0.2"},
 		Port:        "9042",
 		User:        "cassandra",
 		Password:    "cassandra",
@@ -12,6 +12,6 @@ func main() {
 		Keyspace:    "platform_s3_db",
 	}
 
-	cassandra := conn.InitSession()
-	cassandra.Close()
+	cassConn := conn.InitSession()
+	cassConn.Close()
 }
