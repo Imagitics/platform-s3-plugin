ee3061d861b552ed703a57cbfc05766d8b7b4877
nikhilbhide <nikhilbhide15@gmail.com>
2020-06-05 12:53:54 +0530

    added bootstrapping implementation for platform-s3-plugin

--- cmd/s3-plugin/main.go
+++ cmd/s3-plugin/main.go
@@ -5,6 +5,7 @@ import (
 	"github.com/gorilla/mux"
 	"github.com/nik/Imagitics/platform-s3-plugin/infra/cassandra"
 	"github.com/nik/Imagitics/platform-s3-plugin/metadata/repository"
+	"github.com/nik/Imagitics/platform-s3-plugin/utility"
 	"github.com/nik/Imagitics/platform-s3-plugin/web/api/v1"
 	"log"
 	"net/http"
@@ -12,17 +13,25 @@ import (
 )
 
 func main() {
-	//create router for routing the requests
-	router := mux.NewRouter()
+	//load configuration
+	config, err := utility.LoadConfiguration("/etc/config/config.json")
+	if err != nil {
+		//halt bootstrapping
+		fmt.Println("Error in loading configuration - ", err)
+	}
 
-	//create cassandra connection instance
+	fmt.Println(config.Cassandra.Host)
+	fmt.Println(config.Cassandra.Port)
+
+	//instantiate cassandra connection instance
 	conn := &cassandra.CassandraConn{
-		Hosts:       []string{"172.18.0.2"},
-		Port:        "9042",
-		User:        "cassandra",
-		Password:    "cassandra",
-		Consistency: "Quorum",
-		Keyspace:    "platform_s3_db",
+		Hosts:       []string{config.Cassandra.Host},
+		Port:        config.Cassandra.Port,
+		User:        config.Cassandra.User,
+		Password:    config.Cassandra.Password,
+		Consistency: config.Cassandra.Consistency,
+		Keyspace:    config.Cassandra.Keyspace,
+		CaPath:      config.Cassandra.SSLCertPath,
 	}
 
 	//create repoinstance
@@ -30,12 +39,13 @@ func main() {
 	fmt.Println("Connected to Cassandra")
 
 	//instantiate api object and routes
+	router := mux.NewRouter()
 	apiInstnace := v1.NewApi(router, repoInstance)
 	apiInstnace.InitializeRoutes()
 
 	//create a http server
 	srv := &http.Server{
-		Addr: "127.0.0.1:8080",
+		Addr: ":8080",
 		// Good practice: enforce timeouts for servers you create!
 		WriteTimeout: 15 * time.Second,
 		ReadTimeout:  15 * time.Second,
